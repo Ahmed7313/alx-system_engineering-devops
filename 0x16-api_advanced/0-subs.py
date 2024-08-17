5615b62101a35a38332338bbc585898222593334
Ahmed Rabee <35776251+Ahmed7313@users.noreply.github.com>
2024-08-17 20:09:25 +0300

    aaa

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Module for task 0"""
-
+import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, it returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User-Agent for script'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if response.status_code != 200:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    try:
+        data = response.json()
+        return data['data']['subscribers']
+    except (KeyError, ValueError):
+        return 0
